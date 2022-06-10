94d6a12b0b7858bb294db3f01a4942325b779a31
Yoga Saputra <yogsyogs98@gmail.com>
2022-06-10 10:17:13 +0700

    Menambahkan Function, async, dan await

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+  let tampung = ixx.concat(vgc);
+  let totalEmosi = 0;
+  
+  tampung.map(ixx => ixx.hasil === emosi && (totalEmosi += 1) )
+  return totalEmosi
+};
 
 module.exports = {
   promiseOutput,
